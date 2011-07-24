163cd411ef2321ae9c25b6a55ed05332b550c562
boris <bnagaev@gmail.com>
2011-07-24 04:12:39 +0200

    locales_test: add warnings about missing translations

--- locales_test.py
+++ locales_test.py
@@ -7,6 +7,8 @@ import fnmatch
 import os
 import re
 
+ampersand = '-thechess_ampersand'
+
 try:
     wt_messages = parse(open("wt.xml")).getroot()
 except:
@@ -19,9 +21,11 @@ for message in wt_messages:
 
 sections = ['common', 'competition', 'game', 'time', 'user']
 
-Ids = set()
+filename2ids = {}
+id2text = {}
 
 for filename in os.listdir('locales'):
+    ids = filename2ids[filename] = set()
     path = os.path.join('locales', filename)
     xml_file = codecs.open(path, 'r', 'utf-8')
     if xml_file.read().count('\t'):
@@ -30,20 +34,19 @@ for filename in os.listdir('locales'):
     for line in xml_file:
         if len(line) > 120:
             print filename, 'Error: line "%s" is too long' % line
-    xml_file = StringIO(open(path).read().replace('&',''))
+    xml_file = StringIO(open(path).read().replace('&', ampersand))
     messages = parse(xml_file).getroot()
-    keys = set()
     for message in messages:
         Id = message.get('id')
-        Ids.add(Id)
         text = message.text
         if message.tag != 'message':
             print filename, Id, 'Error: wrong tag "%s"' % message.tag
         if text and '\n' not in text and (text[0] == ' ' or text[-1] == ' '):
             print filename, Id, 'Error: message should not start or end with space'
-        if Id in keys:
+        if Id in ids:
             print filename, Id, 'Error: duplicate message'
-        keys.add(Id)
+        ids.add(Id)
+        id2text[Id] = text
         if not Id.startswith('Wt.') and not Id.startswith('tc.'):
             print filename, Id, 'Error: message id should start with Wt. or tc.'
         if Id.startswith('Wt.'):
@@ -65,13 +68,24 @@ for filename in os.listdir('locales'):
         if m1 != m2:
             print filename, 'Error: messages are unsorted, started from %s != %s' % (m1, m2)
 
+all_ids = reduce(lambda a,b:a|b, filename2ids.values(), set())
+cleaner = re.compile(r'{.+}|<.+>|%s.{1,8};' % ampersand)
+
+for filename, ids in filename2ids.items():
+    for Id in all_ids - ids:
+        if filename == 'thechess.xml' and Id.startswith("Wt."):
+            continue
+        if not any(c.isalpha() for c in re.sub(cleaner, "", id2text[Id])):
+            continue
+        print filename, 'Warning: id "%s" not found' % Id
+
 l9n_re = re.compile(r'"(tc\.[^"]+)"')
 
 for root, dirnames, filenames in os.walk('src'):
     for filename in fnmatch.filter(filenames, '*.?pp'):
         path = os.path.join(root, filename)
         for match in re.findall(l9n_re, open(path).read()):
-            if match not in Ids:
+            if match not in all_ids:
                 print path, "Error: can't find message for id '%s'" % match
 
 
