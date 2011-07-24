ded818b79ba4aebc591d83c50665c604508d0205
boris <bnagaev@gmail.com>
2011-07-24 03:43:56 +0200

    locales_test: replace asserts with error messages
    see #26

--- locales_test.py
+++ locales_test.py
@@ -7,7 +7,12 @@ import fnmatch
 import os
 import re
 
-wt_messages = parse(open("wt.xml")).getroot()
+try:
+    wt_messages = parse(open("wt.xml")).getroot()
+except:
+    print 'Error: put wt.xml file into current directory'
+    quit()
+
 wt_ids = set()
 for message in wt_messages:
     wt_ids.add(message.get('id'))
@@ -19,33 +24,46 @@ Ids = set()
 for filename in os.listdir('locales'):
     path = os.path.join('locales', filename)
     xml_file = codecs.open(path, 'r', 'utf-8')
-    assert not xml_file.read().count('\t')
+    if xml_file.read().count('\t'):
+        print filename, 'Error: no tabs are allowed'
     xml_file.seek(0)
     for line in xml_file:
-        assert len(line) <= 120
+        if len(line) > 120:
+            print filename, 'Error: line "%s" is too long' % line
     xml_file = StringIO(open(path).read().replace('&',''))
     messages = parse(xml_file).getroot()
     keys = set()
     for message in messages:
-        assert message.tag == 'message'
         Id = message.get('id')
         Ids.add(Id)
         text = message.text
-        assert not text or '\n' in text or (text[0] != ' ' and text[-1] != ' ')
-        assert Id not in keys
+        if message.tag != 'message':
+            print filename, Id, 'Error: wrong tag "%s"' % message.tag
+        if text and '\n' not in text and (text[0] == ' ' or text[-1] == ' '):
+            print filename, Id, 'Error: message should not start or end with space'
+        if Id in keys:
+            print filename, Id, 'Error: duplicate message'
         keys.add(Id)
-        assert Id.startswith('Wt.') or Id.startswith('tc.')
+        if not Id.startswith('Wt.') and not Id.startswith('tc.'):
+            print filename, Id, 'Error: message id should start with Wt. or tc.'
         if Id.startswith('Wt.'):
-            assert Id in wt_ids
+            if Id not in wt_ids:
+                print filename, Id, 'Error: unknown Wt message (old wt.xml?)'
         else:
             tc, section, id = Id.split('.')
-            assert section in sections
-            assert '-' not in id
-            assert not text or id[0].islower() == text[0].islower() or not text[0].isalpha()
-            if '${' in text:
-                assert id.endswith('_template')
-    short_messages = [m.get('id').lower() for m in messages if m.text and '\n' not in m.text and m.get('id').startswith('tc.')]
-    assert short_messages == sorted(short_messages)
+            if section not in sections:
+                print filename, Id, 'Error: unknown section "%s"' % section
+            if '-' in id:
+                print filename, Id, 'Error: tc id should not contain "-"'
+            if text and id[0].islower() != text[0].islower() and text[0].isalpha():
+                print filename, Id, 'Error: tc id should start with letter of same case as message'
+            if '${' in text and not id.endswith('_template'):
+                print filename, Id, 'Error: id of tc template message should end with "_template"'
+    short_messages = [m.get('id').lower() for m in messages
+        if m.text and '\n' not in m.text and m.get('id').startswith('tc.')]
+    for m1, m2 in zip(short_messages, short_messages):
+        if m1 != m2:
+            print filename, 'Error: messages are unsorted, started from %s != %s' % (m1, m2)
 
 l9n_re = re.compile(r'"(tc\.[^"]+)"')
 
@@ -53,6 +71,8 @@ for root, dirnames, filenames in os.walk('src'):
     for filename in fnmatch.filter(filenames, '*.?pp'):
         path = os.path.join(root, filename)
         for match in re.findall(l9n_re, open(path).read()):
-            assert match in Ids
+            if match not in Ids:
+                print path, "Error: can't find message for id '%s'" % match
+
 
 
