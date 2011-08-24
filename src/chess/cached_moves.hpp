95309dc238b5ab1dd80a9c3b2d662af2c88b2728
Boris Nagaev <bnagaev@gmail.com>
2011-08-24 23:44:30 +0400

    doc chess/cached_moves.hpp

--- src/chess/cached_moves.hpp
+++ src/chess/cached_moves.hpp
@@ -7,7 +7,6 @@
  * See the LICENSE file for terms of use.
  */
 
-//
 #ifndef THECHESS_CACHED_MOVES_H_
 #define THECHESS_CACHED_MOVES_H_
 
@@ -24,20 +23,44 @@ class CachedMoves;
 namespace thechess {
 namespace chess {
 
+/** Board-caching version of Moves */
 class CachedMoves : public Moves {
 public:
+    /** Constructor.
+    \sa Moves::Moves(int moves_count)
+    */
     CachedMoves(int moves_count=0);
-    //CachedMoves(std::string moves);
+
+    /** Construct from Moves */
     CachedMoves(const Moves& moves);
 
+    /** Push the move to the end of container */
     void push_move(Move move);
+
+    /** Pop last move */
     void pop_move();
+
+    /** Pop several moves from the ending */
     void pop_moves(int number);
+
+    /** Strip replace half-move and all its descendants.
+    This method pops several moves (see pop_moves()) and
+    pushes the move (see push_move()).
+    */
     void reset_move(int n, Move move);
 
+    /** Return the half-move */
     Move move_at(int n) const;
+
+    /** Return board position before the half-move */
     const Board& board_at(int n) const;
 
+    /** Apply func to range of half-moves.
+    \param func function to apply
+    \param from index of half-move to begin iteration
+    \param to index of half-move to stop before.
+        If to==-1, iterates from \c from to the end of the container.
+    */
     void foreach(void *func(Move move, const Board& board),
                  int from=0, int to=-1) const;
 
@@ -50,6 +73,5 @@ private:
 
 }
 }
-
 
 #endif // THECHESS_CACHED_MOVES_H_
