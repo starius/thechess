82036ff5d43b6dc33c3c715b87510af04a660bd5
Boris Nagaev <bnagaev@gmail.com>
2011-08-26 15:38:46 +0400

    add model/all.hpp which includes all models

---
+++ src/model/all.hpp
@@ -0,0 +1,23 @@
+
+#ifndef TC_MODEL_ALL_HPP_
+#define TC_MODEL_ALL_HPP_
+
+#include "model/User.hpp"
+#include "model/Game.hpp"
+#include "model/Competition.hpp"
+#include "model/CookieSession.hpp"
+
+namespace thechess {
+
+/** \file model/all.hpp Include this header file from cpp files
+\ingroup model
+*/
+
+/** \defgroup model Database models
+Classes representing models for database.
+*/
+
+}
+
+#endif
+
