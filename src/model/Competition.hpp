c212e3f753d1c1665e469c0491f8f80ada20c162
Boris Nagaev <bnagaev@gmail.com>
2011-10-06 16:15:39 +0300

    start to doc Competition

--- src/model/Competition.hpp
+++ src/model/Competition.hpp
@@ -16,28 +16,51 @@
 
 namespace thechess {
 
+/** 2D map: [white][black] => games vector */
 typedef std::map<UserPtr, std::map<UserPtr, GamesVector> > GamesTable;
 
+/** Model of competition.
+
+\ingroup model
+*/
 class Competition : public dbo::Dbo<Competition> {
 public:
+    /** Type of competition */
     typedef CompetitionType Type;
 
+    /** State of competition */
     enum State {
-        RECRUITING = 10,
-        ACTIVE = 30,
-        ENDED = 50,
-        CANCELLED = 70
+        RECRUITING = 10, /** Recruiting, users are joining */
+        ACTIVE = 30, /** Active, game are played */
+        ENDED = 50, /** Ended, games are finished, winners are attributed */
+        CANCELLED = 70 /** Cancelled */
     };
 
+#ifndef DOXYGEN_ONLY
     Competition();
+#endif
+
+    /** Create instance to be added to database */
     Competition(const CPPtr& cp);
 
+    /** Return string representation of the competition type */
     static Wt::WString type2str(Type type);
+
+    /** Return if all specified games are ended */
     static bool all_ended(const GamesVector& games);
+
+    /** Increase numbers of won game in map.
+    \param games Games to consider
+    \param wins Map (user to number of wins) to modify
+    \note Draw is considered as 0.5 for white and 0.5 for black.
+    */
     static void wins_number(const GamesVector& games,
                             std::map<UserPtr, float>& wins);
+
+    /** Return a vector of users obtained max number of wins */
     static UsersVector winners_of_games(const GamesVector& games);
 
+#ifndef DOXYGEN_ONLY
     template<class Action>
     void persist(Action& a) {
         dbo::belongsTo(a, cp_, "cp");
@@ -53,63 +76,103 @@ public:
         dbo::field(a, ended_, "ended");
         dbo::hasMany(a, winners_, dbo::ManyToMany, "winners_competition");
     }
+#endif
 
+    /** Get competition parameters */
     const CPPtr& cp() const {
         return cp_;
     }
 
+    /** Get type */
     Type type() const;
 
+    /** Get state */
     State state() const {
         return state_;
     }
+
+    /** Return string representation of the competition state */
     static const char* state2str(State state);
+
+    /** Get competition creator */
     const UserPtr& init() const {
         return init_;
     }
 
+    /** Get datetime when competition was created */
     const Wt::WDateTime& created() const {
         return created_;
     }
+
+    /** Get datetime when competition was started */
     const Wt::WDateTime& started() const {
         return started_;
     }
+
+    /** Get datetime when competition was ended */
     const Wt::WDateTime& ended() const {
         return ended_;
     }
 
+    /** Get competition name */
     const Wt::WString& name() const {
         return name_;
     }
+
+    /** Set competition name */
     void set_name(const Wt::WString& v) {
         name_ = v;
     }
+
+    /** Get competition description */
     const Wt::WString& description() const {
         return description_;
     }
+
+    /** Set competition description */
     void set_description(const Wt::WString& v) {
         description_ = v;
     }
 
+    /** Return if the user if a member of the competition */
     bool is_member(const UserPtr& user) const;
+
+    /** Return if the user if a winner of the competition (or one of winners) */
     bool is_winner(const UserPtr& user) const;
 
+    /** Get members collection */
     const Users& members() const {
         return members_;
     }
+
+    /** Get games collection */
     const Games& games() const {
         return games_;
     }
+
+    /** Get winners collection */
     const Users& winners() const {
         return winners_;
     }
 
+    /** Get members vector */
     UsersVector members_vector() const;
+
+    /** Get games vector */
     GamesVector games_vector() const;
+
+    /** Get winners vector */
     UsersVector winners_vector() const;
 
+    /** Calculate games table */
     GamesTable games_table() const;
+
+    /** Return collection of games of the competition played by the user */
     Games games_with(const UserPtr& user) const;
+
+    /** Return vector of games of the competition played by the user.
+    \param gt Table of games is used to avoid database requests.
+    */
     GamesVector games_with(const UserPtr& user, GamesTable& gt) const;
 
     // auto-manage
