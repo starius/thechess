a388af13e472bab937ecec747fbbe04a23c9b745
Boris Nagaev <bnagaev@gmail.com>
2011-10-13 17:11:39 +0300

    model/all.hpp includes all files from model/
    see #83

--- src/model/all.hpp
+++ src/model/all.hpp
@@ -10,12 +10,16 @@
 #ifndef TC_MODEL_ALL_HPP_
 #define TC_MODEL_ALL_HPP_
 
+#include "model/global.hpp"
 #include "model/User.hpp"
 #include "model/GP.hpp"
 #include "model/Game.hpp"
 #include "model/CP.hpp"
 #include "model/Competition.hpp"
 #include "model/CookieSession.hpp"
+#include "model/object.hpp"
+#include "model/elo_player.hpp"
+#include "model/staged_competition.hpp"
 
 namespace thechess {
 
