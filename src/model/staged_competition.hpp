cac6005c4033641c63beb84d30b73d1b480896a8
Boris Nagaev <bnagaev@gmail.com>
2011-10-22 18:17:13 +0300

    doc staged_competition
    --HG--
    extra : rebase_source : 8ea2c69fd4511ad8f1a91878d44c4f3516fb3561

--- src/model/staged_competition.hpp
+++ src/model/staged_competition.hpp
@@ -16,20 +16,38 @@
 
 namespace thechess {
 
+/** Pair of user pointers */
 class UserPair {
 public:
+    /** Create null user pair */
     UserPair();
+
+    /** Constructor.
+    \attention Asserts first != second
+    \note stores min value to first(), max() -- to second()
+    */
     UserPair(const UserPtr& first, const UserPtr& second);
 
+    /** Comparison operator */
     bool operator<(const UserPair& other) const;
+
+    /** Comparison operator */
     bool operator==(const UserPair& other) const;
+
+    /** Comparison operator */
     bool operator!=(const UserPair& other) const;
+
+    /** Boolean operator */
     operator bool() const {
         return first_ && second_;
     }
+
+    /** Get min of two user pointers */
     const UserPtr& first() const {
         return first_;
     }
+
+    /** Get max of two user pointers */
     const UserPtr& second() const {
         return second_;
     }
@@ -39,36 +57,69 @@ private:
     UserPtr second_;
 };
 
+/** Staged competition accessor.
+Instances of this are not stored to database,
+but used to access staged competition-specific data of a competition
+and to process scheduling tasks.
+
+StagedCompetition doesn't reflect Competition's changes.
+*/
 class StagedCompetition {
 public:
+    /** State of member */
     enum State {
         LOSER, PAIRED, WINNER, UNPAIRED
     };
 
+    /** Map User to State */
     typedef std::map<UserPtr, State> States;
+
+    /** Map User to stage */
     typedef std::map<UserPtr, int> Stages;
+
+    /** MultiMap User to UserPair */
     typedef std::multimap<int, UserPair> Paires;
+
+    /** Map UserPair to GamesVector */
     typedef std::map<UserPair, GamesVector> Games;
+
+    /** Map UserPair to User (winner) */
     typedef std::map<UserPair, UserPtr> Winners;
 
+    /** Constructor */
     StagedCompetition(const Competition* competition);
+
+    /** Process scheduling tasks */
     void process(Competition* competition, Objects& objects);
 
+    /** Get states */
     const States& states() const {
         return states_;
     }
+
+    /** Get stages */
     const Stages& stages() const {
         return stages_;
     }
+
+    /** Get paires */
     const Paires& paires() const {
         return paires_;
     }
+
+    /** Get games */
     const Games& games() const {
         return games_;
     }
+
+    /** Get winners of user pairs */
     const Winners& winners() const {
         return winners_;
     }
+
+    /** Get winner (if any) of the competition.
+    Max number of winners of a staged competition is 1.
+    */
     const UserPtr& winner() const {
         return winner_;
     }
