5b885de8c34d9d4d5766dd6ce866903808268a5f
Boris Nagaev <bnagaev@gmail.com>
2012-01-20 23:27:34 +0400

    add class CommentModel

---
+++ src/widgets/comment/CommentModel.cpp
@@ -0,0 +1,40 @@
+/*
+ * thechess, chess game web application written in C++ and based on Wt
+ * Copyright (C) 2010 Boris Nagaev
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#include "widgets/comment/CommentModel.hpp"
+#include "Application.hpp"
+
+namespace thechess {
+
+CommentModel::CommentModel(const CommentPtr& root, Wt::WObject* parent):
+    CommentModel::BaseQM(parent),
+    root_(root) {
+    dbo::Transaction t(tApp->session());
+    setQuery(get_query());
+    addColumn("id", ""); // surrogate field
+    t.commit();
+}
+
+boost::any CommentModel::data(const Wt::WModelIndex& index, int role) const {
+    dbo::Transaction t(tApp->session());
+    if (role == Wt::DisplayRole && index.column() == CONTENTS_COLUMN) {
+        const CommentPtr& o = resultRow(index.row());
+        return contents(o);
+    }
+    return BaseQM::data(index, role);
+}
+
+Wt::WString CommentModel::contents(const CommentPtr& comment) {
+    return comment->text(); // TODO
+}
+
+CommentModel::Query CommentModel::get_query() const {
+    return root_->family().find().where("state <> ?").bind(Comment::SURROGATE);
+}
+
+}
+
---
+++ src/widgets/comment/CommentModel.hpp
@@ -0,0 +1,56 @@
+/*
+ * thechess, chess game web application written in C++ and based on Wt
+ * Copyright (C) 2010 Boris Nagaev
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#ifndef THECHESS_MODEL_COMMENT_MODEL_HPP_
+#define THECHESS_MODEL_COMMENT_MODEL_HPP_
+
+#include <Wt/WGlobal>
+#include <Wt/WString>
+#include <Wt/Dbo/QueryModel>
+
+#include "model/all.hpp"
+
+namespace thechess {
+
+/** MVC model class for comment */
+class CommentModel : public dbo::QueryModel<CommentPtr> {
+public:
+    /** Result type */
+    typedef CommentPtr Result;
+
+    /** Query type */
+    typedef dbo::Query<Result> Query;
+
+    /** Base class of this class */
+    typedef dbo::QueryModel<Result> BaseQM;
+
+    /** Enum for column indices */
+    enum Columns {
+        CONTENTS_COLUMN /**< Contents of the comment */
+    };
+
+    /** Constructor */
+    CommentModel(const CommentPtr& root, Wt::WObject* parent = 0);
+
+    /** Return data at a specific model index */
+    boost::any data(const Wt::WModelIndex& index,
+                    int role = Wt::DisplayRole) const;
+
+    /** Return html comment representation */
+    static Wt::WString contents(const CommentPtr& comment);
+
+    /** Return query for the MVC model */
+    Query get_query() const;
+
+private:
+    CommentPtr root_;
+};
+
+}
+
+#endif
+
