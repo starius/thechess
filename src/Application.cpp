3e8bfba5526c0f11558e5f6224f9134dc0854399
Boris Nagaev <bnagaev@gmail.com>
2012-07-30 20:19:09 +0400

    Application: use Wt layouts
    see #109

--- src/Application.cpp
+++ src/Application.cpp
@@ -13,6 +13,8 @@
 #include <boost/foreach.hpp>
 #include <boost/assert.hpp>
 
+#include <Wt/WVBoxLayout>
+#include <Wt/WScrollArea>
 #include <Wt/WEnvironment>
 #include <Wt/Dbo/Exception>
 #include <Wt/WLogger>
@@ -33,7 +35,9 @@ namespace thechess {
 Application::Application(const Wt::WEnvironment& env, Server& server) :
     Wt::WApplication(env), server_(server), session_(server.pool()),
     gather_(0) {
-    main_widget_ = new MainWidget(root());
+    Wt::WScrollArea* area = new Wt::WScrollArea(root());
+    main_widget_ = new MainWidget();
+    area->setWidget(main_widget_);
     main_widget_->show_menu(&path_);
     path_.connect_main_widget(main_widget_);
     set_auth_widget();
