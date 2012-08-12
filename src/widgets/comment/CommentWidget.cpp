b0e1078b62819e1a7eec785a80c8723d6ead1a1f
Boris Nagaev <bnagaev@gmail.com>
2012-08-12 22:18:20 +0400

    CommentWidget: show header and comment text

--- src/widgets/comment/CommentWidget.cpp
+++ src/widgets/comment/CommentWidget.cpp
@@ -8,12 +8,17 @@
 #include <Wt/WText>
 
 #include "widgets/comment/CommentWidget.hpp"
+#include "widgets/comment/forum_comment_text.hpp"
 #include "widgets/Header.hpp"
 #include "Application.hpp"
 
 namespace thechess {
 
 CommentWidget::CommentWidget(const CommentPtr& comment) {
+    dbo::Transaction t(tApp->session());
+    new Header(tr("tc.forum.Comment"), this);
+    new Wt::WText(forum_comment_text(comment), this);
+    t.commit();
 }
 
 }
