659cbe18b8f315bf276af24d816100cfc7cf10eb
Boris Nagaev <bnagaev@gmail.com>
2012-09-02 21:01:28 +0400

    MainPage: implement main_page_content_id option

--- src/widgets/MainPage.cpp
+++ src/widgets/MainPage.cpp
@@ -8,12 +8,21 @@
 #include <Wt/WText>
 
 #include "widgets/MainPage.hpp"
+#include "Options.hpp"
+#include "Application.hpp"
 
 namespace thechess {
 
 MainPage::MainPage(Wt::WContainerWidget* parent):
     WContainerWidget(parent) {
-    addWidget(new Wt::WText(tr("tc.main.main")));
+    int comment_id = Options::instance()->main_page_content_id();
+    if (comment_id > 0) {
+        dbo::Transaction t(tApp->session());
+        CommentPtr comment = tApp->session().load<Comment>(comment_id);
+        addWidget(new Wt::WText(comment->text()));
+    } else {
+        addWidget(new Wt::WText(tr("tc.main.main")));
+    }
 }
 
 }
