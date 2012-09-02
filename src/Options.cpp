595abd1e22d6acc287782bce921b59aeaa2fa754
Boris Nagaev <bnagaev@gmail.com>
2012-09-02 20:44:29 +0400

    Options: add rights (new, banned, anon) and comments used as main page etc
    inline getters of class Options

--- src/Options.cpp
+++ src/Options.cpp
@@ -19,7 +19,13 @@ Options::Options(const Wt::WServer& server):
     server_(server),
     database_type_(config::defaults::DATABASE_TYPE),
     database_value_(config::defaults::DATABASE_VALUE),
-    connections_in_pool_(config::defaults::CONNECTIONS_IN_POOL) {
+    connections_in_pool_(config::defaults::CONNECTIONS_IN_POOL),
+    regular_user_rights_(REGULAR_USER),
+    banned_ip_user_rights_(NONE),
+    anonymous_rights_(REGULAR_USER),
+    main_page_content_id_(-1),
+    footer_content_id_(-1),
+    top_logged_in_content_id_(-1) {
     std::string value;
     if (server.readConfigurationProperty("database_type", value)) {
         BOOST_ASSERT(value == "postgres" ||
@@ -32,18 +38,12 @@ Options::Options(const Wt::WServer& server):
     }
     server.readConfigurationProperty("database_value", database_value_);
     read_int_value("connections_in_pool", connections_in_pool_);
-}
-
-DatabaseType Options::database_type() const {
-    return database_type_;
-}
-
-const std::string& Options::database_value() const {
-    return database_value_;
-}
-
-int Options::connections_in_pool() const {
-    return connections_in_pool_;
+    read_int_value("regular_user_rights", (int&)(regular_user_rights_));
+    read_int_value("banned_ip_user_rights", (int&)(banned_ip_user_rights_));
+    read_int_value("anonymous_rights", (int&)(anonymous_rights_));
+    read_int_value("main_page_content_id", main_page_content_id_);
+    read_int_value("footer_content_id", footer_content_id_);
+    read_int_value("top_logged_in_content_id", top_logged_in_content_id_);
 }
 
 bool Options::read_int_value(const std::string& name, int& value) {
--- src/Options.hpp
+++ src/Options.hpp
@@ -13,6 +13,7 @@
 #include <Wt/WServer>
 
 #include "thechess-global.hpp"
+#include "model/global.hpp"
 
 namespace thechess {
 
@@ -27,13 +28,53 @@ public:
     Options(const Wt::WServer& server);
 
     /** Get Wt::Dbo backend type */
-    DatabaseType database_type() const;
+    DatabaseType database_type() const {
+        return database_type_;
+    }
 
     /** Get Wt::Dbo backend configuration */
-    const std::string& database_value() const;
+    const std::string& database_value() const {
+        return database_value_;
+    }
 
     /** Get The number of database connections in a pool */
-    int connections_in_pool() const;
+    int connections_in_pool() const {
+        return connections_in_pool_;
+    }
+
+    /** Rights of new user (not from banned IP) */
+    UserRights regular_user_rights() const {
+        return regular_user_rights_;
+    }
+
+    /** Rights of new user (from banned IP) */
+    UserRights banned_ip_user_rights() const {
+        return banned_ip_user_rights_;
+    }
+
+    /** Rights of anonymous.
+    Anonymous doesn't have all possible rights, even if mentioned here.
+    Anonymous with banned IP has bitwise AND of
+    anonymous_rights() and banned_ip_user_rights().
+    */
+    UserRights anonymous_rights() const {
+        return anonymous_rights_;
+    }
+
+    /** Id of comment, which text is used as main page contents */
+    int main_page_content_id() const {
+        return main_page_content_id_;
+    }
+
+    /** Id of comment, which text is used as page footer */
+    int footer_content_id() const {
+        return footer_content_id_;
+    }
+
+    /** Id of comment, which text is shown right to AuthWidget when loggen in */
+    int top_logged_in_content_id() const {
+        return top_logged_in_content_id_;
+    }
 
 private:
     const Wt::WServer& server_;
@@ -41,6 +82,12 @@ private:
     DatabaseType database_type_;
     std::string database_value_;
     int connections_in_pool_;
+    UserRights regular_user_rights_;
+    UserRights banned_ip_user_rights_;
+    UserRights anonymous_rights_;
+    int main_page_content_id_;
+    int footer_content_id_;
+    int top_logged_in_content_id_;
 
     bool read_int_value(const std::string& name, int& value);
 };
