c12b0cd24c85c3baf2f3cf3ebc813720e6867954
Boris Nagaev <bnagaev@gmail.com>
2012-09-09 14:23:37 +0400

    ChatCommentWidget: add links to game, competition or global chat

--- src/widgets/comment/ChatCommentWidget.cpp
+++ src/widgets/comment/ChatCommentWidget.cpp
@@ -6,6 +6,7 @@
  */
 
 #include <Wt/WText>
+#include <Wt/WAnchor>
 #include <Wt/WBreak>
 #include <Wt/WPushButton>
 
@@ -17,6 +18,43 @@
 #include "Application.hpp"
 
 namespace thechess {
+
+Wt::WAnchor* anchor_to_host(const CommentPtr& comment) {
+    dbo::Transaction t(tApp->session());
+    if (comment->type() != Comment::CHAT_MESSAGE) {
+        return 0;
+    }
+    if (!comment->root()) {
+        Wt::WAnchor* a = new Wt::WAnchor();
+        a->setText(Wt::WString::tr("tc.comment.Global_chat"));
+        a->setLink(tApp->path().global_chat()->link());
+        return a;
+    }
+    try {
+        GamePtr game = tApp->session().find<Game>()
+                       .where("comment_base_id = ?").bind(comment->root());
+        if (game) {
+            Wt::WAnchor* a = new Wt::WAnchor();
+            a->setText(Wt::WString::tr("tc.game.Header").arg((int)game.id()));
+            a->setLink(tApp->path().game_view()->get_link(game.id()));
+            return a;
+        }
+    } catch (...)
+    { }
+    try {
+        CompetitionPtr c = tApp->session().find<Competition>()
+                           .where("comment_base_id = ?").bind(comment->root());
+        if (c) {
+            Wt::WAnchor* a = new Wt::WAnchor();
+            a->setText(Wt::WString::tr("tc.competition.Header")
+                       .arg((int)c.id()));
+            a->setLink(tApp->path().competition_view()->get_link(c.id()));
+            return a;
+        }
+    } catch (...)
+    { }
+    return 0;
+}
 
 ChatCommentWidget::ChatCommentWidget(const CommentPtr& comment) {
     dbo::Transaction t(tApp->session());
@@ -30,6 +68,11 @@ ChatCommentWidget::ChatCommentWidget(const CommentPtr& comment) {
     if (user) {
         new Wt::WBreak(this);
         user_anchor(user, this);
+    }
+    Wt::WAnchor* a = anchor_to_host(comment);
+    if (a) {
+        new Wt::WBreak(this);
+        addWidget(a);
     }
     if (comment->type() == Comment::CHAT_MESSAGE) {
         add_remover_buttons(comment, this);
