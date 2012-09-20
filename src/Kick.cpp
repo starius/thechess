ba80b873675958880f788d393b42741598f56b2b
Boris Nagaev <bnagaev@gmail.com>
2012-09-20 12:13:15 +0400

    Kick: show message

--- src/Kick.cpp
+++ src/Kick.cpp
@@ -19,11 +19,27 @@ Kick::Kick():
 { }
 
 static void logout_app() {
+    dbo::Transaction t(tApp->session());
+    bool removed = true;
+    if (tApp->user()) {
+        tApp->user().reread();
+        if (!tApp->user()->removed()) {
+            removed = false;
+        }
+    }
     tApp->session().login().logout();
+    wApp->root()->clear();
+    if (removed) {
+        new Wt::WText(Wt::WString::tr("tc.user.Removed_text"), wApp->root());
+    } else {
+        new Wt::WText(Wt::WString::tr("tc.user.Kicked_text"), wApp->root());
+    }
     wApp->triggerUpdate();
+    wApp->quit();
 }
 
 void Kick::notify(EventPtr) {
+    dbo::Transaction t(tApp->session());
     if (tApp->user()) {
         Wt::Wc::bound_post(logout_app)();
     }
